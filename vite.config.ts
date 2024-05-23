eefe43f618016fdb07195c77c302623fee6ac762
illiaBabak <illiababak776@gmail.com>
2024-05-23 23:27:33 +0200

    Preparation to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/crud-json-placeholder',
   build: {
     outDir: 'build',
     emptyOutDir: true,
